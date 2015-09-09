1dcebf51c47f566107e5ebba52d037a11380340d
Vova Abdrakhmanov <369565@gmail.com>
2015-09-09 15:29:49 +0300

    add derivativeTree

--- main.d
+++ main.d
@@ -24,7 +24,7 @@ BinaryTree!token * expressionToTree(string expression) {
                     holder.pop();
                 }
                 holder.pop();
-                if (holder.top.type == tokenType.FUNCTION) {
+                if (!holder.empty && holder.top.type == tokenType.FUNCTION) {
                     output.push(holder.top);
                     holder.pop();
                 }
@@ -114,6 +114,143 @@ string treeToExpression(BinaryTree!token * tree) {
     return result;
 }
 
+void derivativeTree(BinaryTree!token * tree, uint varId) {
+    switch(tree.value.type) {
+        case tokenType.NUMBER:
+            tree.value.id = addToTable(tableNumbers, 0);
+            break;
+        case tokenType.ID:
+            tree.value.type = tokenType.NUMBER;
+            if (tree.value.id == varId)
+                tree.value.id = addToTable(tableNumbers, 1);
+            else
+                tree.value.id = addToTable(tableNumbers, 0);
+            break;
+        case tokenType.OPERATOR:
+            switch(tree.value.id) {
+                case 0:
+                case 1:
+                    derivativeTree(tree.left, varId);
+                    derivativeTree(tree.right, varId);
+                    break;
+                case 2:
+                    auto ldup = tree.dup(tree);
+                    auto rdup = tree.dup(tree);
+                    tree.value.id = 0;
+                    derivativeTree(ldup.left, varId);
+                    derivativeTree(rdup.right, varId);
+                    tree.left = ldup;
+                    tree.right = rdup;
+                    break;
+                case 3:
+                    auto ldup = tree.dup;
+                    auto rdup = tree.dup;
+                    ldup.value.id = 2;
+                    rdup.value.id = 2;
+                    derivativeTree(ldup.left, varId);
+                    derivativeTree(rdup.right, varId);
+                    tree.right = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 4), tree, tree.right);
+                    tree.right.right = new BinaryTree!token(
+                        token(tokenType.NUMBER, addToTable(tableNumbers, 2)),
+                        tree.right);
+                    tree.left = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 1), tree, ldup, rdup);
+                    ldup.parent = tree.left;
+                    rdup.parent = tree.left;
+                    break;
+                case 4:
+                    auto l1 = tree.left.dup;
+                    auto l2 = tree.left.dup;
+                    auto l3 = tree.left.dup;
+                    auto l4 = tree.left.dup;
+                    auto r1 = tree.right.dup;
+                    auto r2 = tree.right.dup;
+                    auto r3 = tree.right.dup;
+                    auto r4 = tree.right.dup;
+                    tree.value.id = 0;
+                    tree.left = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 2), tree);
+
+                    tree.left.left = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 4), tree.left);
+
+                    tree.left.left.left = l1;
+                    l1.parent = tree.left.left;
+
+                    tree.left.left.right = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 1), tree.left.left);
+
+                    tree.left.left.right.left = r1;
+                    r1.parent = tree.left.left.right;
+
+                    tree.left.left.right.right = new BinaryTree!token(
+                            token(tokenType.NUMBER, addToTable(tableNumbers,
+                                    1)), tree.left.left.right);
+
+                    derivativeTree(l2, varId);
+                    tree.left.right = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 2), tree.left, l2, r2);
+                    l2.parent = tree.left.right;
+                    r2.parent = tree.left.right;
+
+
+                    tree.right = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 2), tree);
+
+                    tree.right.left = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 4), tree.right, l3, r3);
+                    l3.parent = tree.right.left;
+                    r3.parent = tree.right.left;
+
+                    tree.right.right = new BinaryTree!token(
+                            token(tokenType.OPERATOR, 2), tree.right);
+                    tree.right.right.left = new BinaryTree!token(
+                            token(tokenType.FUNCTION, 3), tree.right.right, l4);
+                    l4.parent = tree.right.right.left;
+
+                    derivativeTree(r4, varId);
+                    tree.right.right.right = r4;
+                    r4.parent = tree.right.right;
+                    break;
+                default: break;
+            }
+        case tokenType.FUNCTION:
+            auto copy = tree.dup(tree);
+            tree.value.type = tokenType.OPERATOR;
+            switch(tree.value.id) {
+                case 0:
+                case 1:
+                case 2:
+                    tree.value.id = 2;
+                    tree.right = tree.left;
+                    derivativeTree(tree.right, varId);
+                    tree.left = copy;
+                    if (tree.left.value.id == 0) {tree.left.value.id = 1;}
+                    else if (tree.left.value.id == 1) {
+                        tree.left.value.id = 0;
+                        auto newcopy = tree.dup;
+                        auto minusOne = new
+                            BinaryTree!token(token(tokenType.NUMBER,
+                                        addToTable(tableNumbers, -1)));
+                        tree.value = token(tokenType.OPERATOR, 2);
+                        tree.left = minusOne;
+                        tree.right = newcopy;
+                        tree.left.parent = tree;
+                        tree.right.parent = tree;
+                    }
+                    break;
+                case 3:
+                    tree.value.id = 3;
+                    tree.right = copy.left;
+                    derivativeTree(tree.left, varId);
+                    break;
+                default: break;
+            }
+        default: break;
+    }
+}
+
 void simplifyTree(BinaryTree!token * tree) {
     if (tree.left != null)
         simplifyTree(tree.left);
@@ -161,9 +298,10 @@ void simplifyTree(BinaryTree!token * tree) {
 void main() {
     writeln("Введите выражение:");
     auto expression = readln()[0 .. $-1]; // хак, чтобы не читать символ переноса строки
+    writeln("Введите переменную, по которой дифференцировать:");
+    auto var = readln()[0 .. $-1];
+    auto varId = addToTable(tableIds, var);
     auto tree = expressionToTree(expression);
-    auto tree1 = tree.dup;
-    simplifyTree(tree);
+    derivativeTree(tree, varId );
     writeln(treeToExpression(tree));
-    writeln(treeToExpression(tree1));
 }
