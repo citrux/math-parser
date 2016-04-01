bfbd04ebbf6f35a029aba2d933fe76d6cb97253e
Alexey Golubev <dr.freecx@gmail.com>
2016-04-01 21:23:57 +0300

    [upd] rewrite part of lexer

--- source/lexer.d
+++ source/lexer.d
@@ -6,6 +6,8 @@ module lexer;
 import std.regex;
 import std.conv;
 
+import std.stdio;
+
 enum tokenType {
     ERROR,
     NUMBER,
@@ -69,59 +71,49 @@ uint addToTable(T)(ref T[] table, T value) {
 struct tokenRange {
     auto scanner = ctRegex!(`(\s+)|(\d+\.\d+|\d+)|([-*+/^])|(sin|cos|exp|ln)|([A-Za-z_][A-Za-z0-9_]*)|(\()|(\))|(.)`);
 
-    // что за фигня с этими типами?
-    typeof(matchAll("", scanner)) matchRange;
-
+    token[] matchRange;
+    uint index = 0;
 
     this(string expression) {
-        matchRange = matchAll(expression, scanner);
+        auto tokType = tokenType.ERROR;
+        uint id;
+        foreach(m; matchAll(expression, scanner)) {
+            // всё ещё говнокод)
+            if (m[2].length) {
+                tokType = tokenType.NUMBER;
+                id = addToTable(tableNumbers, to!double(m[0]));
+            }
+            if (m[3].length) {
+                tokType = tokenType.OPERATOR;
+                id = addToTable(tableOperators, m[0]);
+            }
+            if (m[4].length) {
+                tokType = tokenType.FUNCTION;
+                id = addToTable(tableFunctions, m[0]);
+            }
+            if (m[5].length) {
+                tokType = tokenType.ID;
+                id = addToTable(tableIds, m[0]);
+            }
+            if (m[6].length) {
+                tokType = tokenType.OPEN_PAR;
+            }
+            if (m[7].length) {
+                tokType = tokenType.CLOSE_PAR;
+            }
+            matchRange ~= token(tokType, id);
+        }
     }
 
     @property token front() {
-        auto m = matchRange.front();
-        auto tokType = tokenType.ERROR;
-        uint id;
-
-        // говнокооооооддддд))))
-        if (m[1].length) {
-            matchRange.popFront();
-            m = matchRange.front();
-        }
-
-        if (m[2].length) {
-            tokType = tokenType.NUMBER;
-            id = addToTable(tableNumbers, to!double(m[0]));
-        }
-
-        if (m[3].length) {
-            tokType = tokenType.OPERATOR;
-            id = addToTable(tableOperators, m[0]);
-        }
-
-        if (m[4].length) {
-            tokType = tokenType.FUNCTION;
-            id = addToTable(tableFunctions, m[0]);
-        }
-
-        if (m[5].length) {
-            tokType = tokenType.ID;
-            id = addToTable(tableIds, m[0]);
-        }
-
-        if (m[6].length)
-            tokType = tokenType.OPEN_PAR;
-
-        if (m[7].length)
-            tokType = tokenType.CLOSE_PAR;
-
-        return token(tokType, id);
+        return matchRange[index];
     }
 
     void popFront () {
-        matchRange.popFront();
+        index += 1;
     }
 
     @property bool empty() {
-        return matchRange.empty;
+        return matchRange.length <= index;
     }
 }
